485042beef6c93bc2af08d4117f80d39ca25e550
Mashenka737 <kataeva.Maria737@yandex.ru>
2014-09-24 22:58:38 +0400

    it's uncompiled files

--- sqr_equation/SQR_equation.cpp
+++ sqr_equation/SQR_equation.cpp
@@ -1,21 +1,85 @@
 #include "equationLib.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-void read (double &a, double &b, double &c);
-
-void solve (double a, double b, double c, double &x1, double &x2);
-
-void write_solution (double x1, double x2);
-
-int main()
+void read_coefficient (double &n)
 {
-    double a = 0, b = 0, c = 0;
-    double x1 = 0, x2 = 0;
+    int i = 0;
+    i = scanf ("%lg", &n);
+    while (i == 0)
+    {
+        printf("It's a mistake here. Try again, please!\n");
+        fflush(stdin);
+        i = scanf ("%lg,", &n);
+    }
+}
 
-    read(a,b,c);
-    solve(a, b, c, x1, x2);
-    write_solution(x1, x2);
+void read (double &a, double &b, double &c)
+{
+    printf("Hello! You can solve the equation form a*x^2 + b*x +c = 0 with this program.\nSo,\n");
 
-    return 0;
+    printf("enter the coefficient a\n");
+    read_coefficient(a);
+    printf("enter the coefficient b\n");
+    read_coefficient(b);
+    printf("enter the coefficient c\n");
+    read_coefficient(c);
+}
+
+Number solve (double a, double b, double c, double &x1, double &x2)
+{
+    double d = 0;
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+                solve = INFINITE_SOLUTIONS;
+            else
+                solve = NO_SOLUTION;
+        }
+        else
+        {
+            x1 = -c/b;
+            solve = ONE_SOLUTION;
+        }
+    }
+    else
+    {
+        d = b*b - 4*a*c;
+        if (d < 0)
+            solve = NO_SOLUTION;
+        else
+        {
+            if (d > 0)
+            {
+                x1 = (-b - sqrt(d))/(2*a);
+                x2 = (-b + sqrt(d))/(2*a);
+                solve = TWO_SOLUTIONS;
+            }
+            else
+            {
+                x1 = -b/(2*a);
+                solve = ONE_SOLUTION;
+            }
+        }
+    }
+}
+
+void write_solution (double x1, double x2, Nomber num)
+{
+   switch (num)
+    {
+        case NO_SOLUTION:
+            printf("No solution\n");
+            break;
+        case INFINITE_SOLUTIONS:
+            printf("Infinite solutions\n");
+            break;
+        case ONE_SOLUTION:
+            printf("The solution of equation is \nx = %lg", x1);
+            break;
+        case TWO_SOLUTIONS:
+            printf("The solutions of equation are \nx1 = %lg\nx2 = %lg", x1, x2);
+            break;
+        default: break;
+    }
 }
--- sqr_equation/equationLib.h
+++ sqr_equation/equationLib.h
@@ -1,89 +1,14 @@
+#ifndef EQUATIONLIB_H
+#define EQUATIONLIB_H
+
+#include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
 
 enum Number {NO_SOLUTION, INFINITE_SOLUTIONS, ONE_SOLUTION, TWO_SOLUTIONS};
-Number num;
+void read_coefficient (double &n);
+void read (double &a, double &b, double &c);
+Number solve (double a, double b, double c, double &x1, double &x2);
+void write_solution (double x1, double x2, Number num);
 
-void read_coefficient (double &n)
-{
-    int i = 0;
-    i = scanf ("%lg", &n);
-    while (i == 0)
-    {
-        printf("It's a mistake here. Try again, please!\n");
-        fflush(stdin);
-        i = scanf ("%lg,", &n);
-    }
-}
-
-void read (double &a, double &b, double &c)
-{
-    printf("Hello! You can solve the equation form a*x^2 + b*x +c = 0 with this program.\nSo,\n");
-
-    printf("enter the coefficient a\n");
-    read_coefficient(a);
-    printf("enter the coefficient b\n");
-    read_coefficient(b);
-    printf("enter the coefficient c\n");
-    read_coefficient(c);
-}
-
-void solve (double a, double b, double c, double &x1, double &x2)
-{
-    double d = 0;
-    if (a == 0)
-    {
-        if (b == 0)
-        {
-            if (c == 0)
-                num = INFINITE_SOLUTIONS;
-            else
-                num = NO_SOLUTION;
-        }
-        else
-        {
-            x1 = -c/b;
-            num = ONE_SOLUTION;
-        }
-    }
-    else
-    {
-        d = b*b - 4*a*c;
-        if (d < 0)
-            num = NO_SOLUTION;
-        else
-        {
-            if (d > 0)
-            {
-                x1 = (-b - sqrt(d))/(2*a);
-                x2 = (-b + sqrt(d))/(2*a);
-                num = TWO_SOLUTIONS;
-            }
-            else
-            {
-                x1 = -b/(2*a);
-                num = ONE_SOLUTION;
-            }
-        }
-    }
-}
-
-void write_solution (double x1, double x2)
-{
-   switch (num)
-    {
-        case NO_SOLUTION:
-            printf("No solution\n");
-            break;
-        case INFINITE_SOLUTIONS:
-            printf("Infinite solutions\n");
-            break;
-        case ONE_SOLUTION:
-            printf("The solution of equation is \nx = %lg", x1);
-            break;
-        case TWO_SOLUTIONS:
-            printf("The solutions of equation are \nx1 = %lg\nx2 = %lg", x1, x2);
-            break;
-        default: break;
-    }
-}
+#endif
